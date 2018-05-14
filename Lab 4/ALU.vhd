fe4643e6bd4e8e6d27d0d841ce1cca6b077a0c3f
franklin <franklinokomba@gmail.com>
2018-05-14 16:31:07 -0700

    second commit

--- Lab 4/ALU.vhd
+++ Lab 4/ALU.vhd
@@ -34,9 +34,52 @@ architecture ALU_Arch of ALU is
 			dataout: out std_logic_vector(31 downto 0));
 	end component shift_register;
 
-begin
-	-- Add ALU VHDL implementation here
 
+	-- Add ALU VHDL implementation here
+	signal outputSign, temp,adder,subtractor,OR_out, AND_out,andi, addi,ori,shift: std_logic_vector(31 downto 0);
+	signal passThrough: std_logic_vector(31 downto 0);
+	signal carry: std_logic;
+	signal ori_temp3,ori_temp2,andi_temp3,andi_temp2: std_logic_vector(19 downto 0);
+begin
+	
+	add1: adder_subtracter port map (DataIn1,DataIn2,'0',adder,carry); --Port map for adding the 32 bit numbers
+	add2: adder_subtracter port map (DataIn1,DataIn2,'1',subtractor,carry); --Port map subtracting the 32 bit numbers
+	
+	OR_out<=DataIn1 or DataIn2;	    --or the 32 bit numbers
+	AND_out<=DataIn1 and DataIn2;	--and the 32 bit numbers
+	passThrough <= DataIn2;         -- DataIn2 pass through signal
+
+	     ---OR immediate
+	ori_temp2<= DataIn2(19 downto 0);
+	ori_temp3<= DataIn1(19 downto 0) or ori_temp2;
+	ori<=DataIn1(31 downto 20) & ori_temp3 ;
+
+	     -----AND immediate
+	andi_temp2<= DataIn2(19 downto 0);
+	andi_temp3<= DataIn1(19 downto 0) and andi_temp2;
+	andi<=DataIn1(31 downto 20) & andi_temp3 ;
+	
+	add3: adder_subtracter port map (DataIn1,temp, '0' ,addi,carry);
+
+	shift1: shift_register port map(DataIn1,ALUCtrl(0),DataIn2(10 downto 6),shift);
+
+
+	with ALUCtrl select
+		outputSign<= adder when "00000",
+			subtractor when "00001",
+			AND_out when "00010",
+			OR_out when "00101",
+			addi when "00110",
+			andi when "01010",
+			ori when "01101",
+			passThrough when "01111",
+			shift when others;
+
+	with outputSign select
+		Zero<= '1' when X"00000000",
+			'0' when others;
+
+	ALUResult<=outputSign;
 end architecture ALU_Arch;
 
 
--- Lab 4/tALU.vhd
+++ Lab 4/tALU.vhd
@@ -50,14 +50,60 @@ BEGIN
 		wait for 100 ns;
 
 		-- Start testing the ALU
-		datain_a <= X"01234567";	-- DataIn in hex
-		datain_b <= X"11223344";
+		-- test the adder_subtracter first
+		datain_a <= X"4F302C85";
+		datain_b <= X"7A222578";
 		control  <= "00000";		-- Control in binary (ADD and ADDI test)
-		wait for 20 ns; 			-- result = 0x124578AB  and zeroOut = 0
+		wait for 20 ns; 		    -- dataout should be 0xC95251FD and zeroOut = 0
 
-		-- Add test cases here to drive the ALU implementation
+		datain_a <= X"4F302C85";
+		datain_b <= X"7A222578";	   -- subtraction at 120nS
+		control  <= "00001";          -- dataout should be 0xD50E070D		
+		wait for 20 ns;               
+		
+		datain_a <= X"FFFFFFFF";	--and all 1's and 0's
+		datain_b <= X"00000000";	--after 140
+		control  <= "00010";		
+		wait for 20 ns; 
 
+		datain_a <= X"FFFFFFFF";	--or all 1's and 0's
+		datain_b <= X"00000000";	--after 160
+		control  <= "00101";		
+		wait for 20 ns;
 
+	
+		datain_a <= X"00000000";	--adding immediate to zero
+		datain_b <= X"00FFFFFF";	--after 180
+		control  <= "00110";		
+		wait for 20 ns;
+	
+		
+		datain_a <= X"00000000";	-- oring numbers immidates
+		datain_b <= X"FFFFFFFF";	--after 200
+		control  <= "01101";		
+		wait for 20 ns; 
+
+		--datain_a <= X"00000000";	-- Data in 2 pass through line
+		datain_b <= X"11111111";	--after 220
+		control  <= "01111";		
+		wait for 20 ns; 	
+		
+		datain_a <= X"FFFFFFFF";	-- shift left by 2
+		datain_b <= X"00000080";	--after 240
+		control  <= "11110";		
+		wait for 20 ns; 	
+			
+		datain_a <= X"0FFFFFFF";	-- shift right by 2
+		datain_b <= X"00000080";	--after 260
+		control  <= "11111";		
+		wait for 20 ns;
+
+        datain_a <= X"00000000";	-- shift right by 2
+		datain_b <= X"FFFFFFFF";	--after 280
+		control  <= "01010";		
+		wait for 20 ns; 		
+	
+			
 		wait; -- will wait forever
 	END PROCESS;
 
