9ed4d6fd816aacf58a515f55b4bbfae8236097f3
franklin <franklinokomba@gmail.com>
2018-05-14 17:00:34 -0700

    third commit

--- Lab 4/part3.asm
+++ Lab 4/part3.asm
@@ -5,29 +5,43 @@
 .text
 
 main:	
-	li t0 13
+	li t0 0x4f302c85
 
-	li t1 11
+	li t1 0x7a222578
 
-	li t2 8
-
-	li t3 18
+	
 
 	add s0 t0 t1
-	addi s1 t0 11
 	
+	li t0 0x4f302c85
+	li t1 0x7a222578
 	sub s1 t0 t1
 
-	and s2 t0 t1
-	andi s3 t0 11 
 	
-	or s4 t0 t1
+	li t0 0x00000000
+	addi s2 t0 0x000f
+	
+	
+	li t0 0xffffffff
+	li t1 0x00000000
+	and s3 t0 t1
+	
+	li t0 0x00000000
+	andi s4 t0 0xffffffff 
+	
+	
+	li t0 0x00000000
+	li t1 0xffffffff
+	or s5 t0 t1
 
-	ori s5 t0 11
+	li t0 0x00000000
+	ori s6 t0 0xffffffff
 
-	slli s6 t0 11
+	li t0 0xffffffff
+	li t1 0x00000080
+	sll s7 t0 t1
 
-	srli s7 t0 11
+	srl s8 t0 t1
 
 
 	li a7, 10
